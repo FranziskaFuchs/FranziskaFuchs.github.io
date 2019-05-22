3270e39d23ff32d90e9d7d8475104e8af978c921
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2019-05-22 13:45:38 +0200

    Update script.ts

--- Type Script/script.ts
+++ Type Script/script.ts
@@ -2,3 +2,32 @@ console.log("Dokument wird geladen");
 
 window.onload=function() {
 console.log("Dokument ist geladen");
+document.getElementById("Ramona").addEventListener("click", changeName);
+}
+
+function changeName()
+{
+    let id: string="time";
+
+    let stringRamona: string="";
+    let time: string= "eleven"
+    let number1: number=1;
+    let number2: number=2;
+
+    number1=3;
+    number1 = number1 + 2;
+    number1 += 2;
+
+    name += name;
+    console.log(name);
+
+    console.log("Dies Variable ist leer: " + stringRamona);
+
+    let love: number=number1 + number2;
+    let Heart: heart=true;
+
+
+
+
+}
+
