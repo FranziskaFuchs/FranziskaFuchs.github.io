eee9f004783969811e61077719ab380539b1ff90
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2019-05-25 09:45:49 +0200

    7

--- Type Script/script.js
+++ Type Script/script.js
@@ -1,20 +1,32 @@
-console.log("Dokument wird geladen");
+console.log("Dokument wird geladen"); /*Konsolenausgabe nach Start */
 window.onload = function () {
     console.log("Dokument ist geladen");
     document.getElementById("Ramona").addEventListener("click", changeName);
+    function changeName() {
+        let str1 = "time";
+        let str2 = "eleven";
+        let number1 = 1;
+        let number2 = 2;
+        number1 = 2;
+        number1 = number1 + number2;
+        console.log(number1);
+        str1 = str1 + str2;
+        console.log(str1);
+        console.log(number1 + str1);
+        document.getElementById("time").innerHTML += " things";
+        document.getElementById("time");
+        className = "tight";
+        function init() {
+            document.getElementById('button').addEventListener('click', hinzufuegen);
+        }
+        function hinzufuegen() {
+            var typ = document.Formular.Elementtyp.options[document.Formular.Elementtyp.selectedIndex].value, elementknoten = document.createElement(typ);
+            if (typ != 'hr') {
+                var textknoten = document.createTextNode(document.Formular.Elementinhalt.value);
+                elementknoten.appendChild(textknoten);
+            }
+            document.getElementById('User').appendChild(elementknoten);
+        }
+    }
 };
-function changeName() {
-    let str1 = "time";
-    let str2 = "eleven";
-    let number1 = 1;
-    let number2 = 2;
-    number1 = number1 + number2;
-    console.log(number1);
-    str1 = str1 + str2;
-    console.log(str1);
-    console.log(number1 + str1);
-    document.getElementById("time").innerHTML += " things";
-    document.getElementById("time");
-    className = "tight";
-}
 //# sourceMappingURL=script.js.map
--- Type Script/script.ts
+++ Type Script/script.ts
@@ -1,9 +1,9 @@
-console.log("Dokument wird geladen");
+console.log("Dokument wird geladen"); /*Konsolenausgabe nach Start */
 
 window.onload=function() {
 console.log("Dokument ist geladen");
-document.getElementById("Ramona").addEventListener("click", changeName);
-}
+document.getElementById("Ramona").addEventListener("click", changeName); 
+
 
 function changeName()
 {
@@ -12,7 +12,8 @@ function changeName()
     let number1: number=1;
     let number2: number=2;
 
-    number1= number1+number2;
+    number1= 2;
+    number1= number1+number2; 
     console.log(number1);
 
     str1=str1+str2;
@@ -23,6 +24,20 @@ function changeName()
 
     document.getElementById("time").innerHTML += " things";
     document.getElementById("time")className = "tight";
+
+    function init() {
+        document.getElementById('button').addEventListener('click', hinzufuegen);
+      }
+     
+      function hinzufuegen () {
+        var typ = document.Formular.Elementtyp.options[document.Formular.Elementtyp.selectedIndex].value,
+            elementknoten = document.createElement(typ);
+        if (typ != 'hr') {
+           var textknoten = document.createTextNode(document.Formular.Elementinhalt.value);
+           elementknoten.appendChild(textknoten);
+        }
+          document.getElementById('User').appendChild(elementknoten);
+        }
 
 
 
