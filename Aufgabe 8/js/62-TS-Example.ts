4b976121975704dc9c35af6628594e05f7889ee2
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2019-05-31 14:45:47 +0200

    8

--- Aufgabe 8/js/62-TS-Example.ts
+++ Aufgabe 8/js/62-TS-Example.ts
@@ -53,7 +53,11 @@ console.log(monsterArray); // Gebe das Monster-Array einmal zu beginn aus. Es so
 
 // Generelle onload-funktion um Event-Listener zum Dokument hinzuzufügen
 window.onload = function () {
-    document.getElementById("monsterSpawner").addEventListener("click", generateMonster,true);
+    document.getElementById("monsterSpawner").addEventListener("click", generateMonster,false);
+
+function generateMonster (){
+    document.getElementById("monsterSpawner").className ="false";
+}
     updatePlayerLevel(); // Zu Anfang wird durch eine Funktion ein HTML-Element mit Inhalt befüllt.
 }
 
